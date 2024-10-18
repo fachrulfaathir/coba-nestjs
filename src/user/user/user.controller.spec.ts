d25fc6d67dfb7a3d1955b4fcf65582e48dce0578
Fachrul Faathirullah <fachrulfaathir36@gmail.com>
2024-10-18 14:46:48 +0700

    testing dengan node mocks http

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+const httpMocks = require('node-mocks-http');
+
 
 describe('UserController', () => {
   let controller: UserController;
@@ -15,4 +17,23 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it("should say hello", async () =>{
+    const response = await controller.sayHello("Fachrul", "Faathirullah");
+    expect(response).toBe("Hello Fachrul Faathirullah")
+  })
+
+
+  it("should can get view", async () => {
+    const response = httpMocks.createResponse();
+    controller.viewHello("Fachrul", "Faathirullah", response);
+
+    expect(response._getRenderView()).toBe("index.html");
+    expect(response._getRenderData()).toEqual({
+      name: "Fachrul",
+      last_name: "Faathirullah",
+      title: "Template Engine"
+    })
+
+  })
 });
