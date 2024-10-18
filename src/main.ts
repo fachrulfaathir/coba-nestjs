a7a1bba136b932294ed26d3d010b99ef20aa7853
Fachrul Faathirullah <fachrulfaathir36@gmail.com>
2024-10-18 14:17:11 +0700

    view, cookie , asynchronous

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+const cookieParser = require('cookie-parser');
+import * as mustache from "mustache-express";
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser("Rahasia"));
+  app.set("views", __dirname + "/../views")
+  app.set("view engine", "html")
+  app.engine("html", mustache())
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,79 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Header, HttpCode, HttpRedirectResponse, Param, Post, Query, Redirect, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('/api/user')
-export class UserController {}
+export class UserController {
+
+    @Post("/post/sample")
+    post() : string {
+        return 'Sample response'; 
+    }
+
+    @Get("/get/sample")
+    get() : string {
+        return 'Sample response'; 
+
+    }
+
+
+    @Get("/hello")
+    async sayHello(
+        @Query("first_name") first_name : string,
+        @Query("last_name") last_name : string
+    ) : Promise<string> {
+       return `Hello ${first_name + " "+ last_name}`
+    }
+
+    
+    @Get("/id/:id")
+    getById(@Param("id") id : string) : string {
+       return `Get ${id}`
+    }
+
+    @Post("/body")
+    getByBody(@Body("name") name : string) : string {
+       return `Get ${name}`
+    }
+
+    @Get("/sample-response")
+    @Header("Content-Type", "application/json")
+    @HttpCode(200)
+    sampleResponse() : Record<string,string> {
+       return {
+        "data" : "Hello JSON" 
+       }
+    }
+
+    @Get("/redirect")
+    @Redirect()
+    redirect(): HttpRedirectResponse{
+        return {
+            url : "/api/user/sample-response",
+            statusCode: 302
+        }
+    }
+
+    @Get("/set-cookie")
+    @Header("Content-Type", "application/json")
+    sampleCookies(@Query("name") name:string, @Res() response:Response){
+        response.cookie("name", name);
+        response.status(200).send("Success set status cookie");
+
+    }
+
+    @Get("/get-cookie")
+    setCookie(@Req() request:Request) : string {
+        return request.cookies['name'];
+    }
+
+    @Get("/view/hello")
+    viewHello(@Query("name") name: string, @Query("last_name") last_name: string, @Res() response: Response) {
+        return response.render("index.html", {
+            title: "Template Engine",
+            name: name,
+            last_name:last_name
+        });
+    }
+    
+
+}
